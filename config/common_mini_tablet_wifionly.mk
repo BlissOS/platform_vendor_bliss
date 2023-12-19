f761305fd1274efcf21781091b8481a0921cb38b
hmtheboy154 <buingoc67@gmail.com>
2023-12-19 11:01:07 -0500

    Don't use window extensions for mini tablets
    Change-Id: Ic9ef0881a43a3e416146df58296f2eb9cfe23020
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- config/common_mini_tablet_wifionly.mk
+++ config/common_mini_tablet_wifionly.mk
@@ -1,5 +1,3 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
-
 # Inherit mobile mini common Bliss stuff
 $(call inherit-product, vendor/bliss/config/common_mobile_mini.mk)
 
